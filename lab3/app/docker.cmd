85624181a8102c402f9f58d18d3b9dc32ba9abec
Evgeniy Zotov <e.zotov@carx-tech.com>
2021-05-23 16:29:14 +0700

    Настройки для prometheus

--- lab3/app/docker.cmd
+++ lab3/app/docker.cmd
@@ -1,4 +1,7 @@
 REM Собрать образ
-docker build -t zotovkem86/car-otus:v4 .
+docker build -t zotovkem86/car-otus:v5 .
 REM Отправка образа в репозеторий
-docker push zotovkem86/car-otus:v4
+docker push zotovkem86/car-otus:v5
+REM Сделать нагрузку на приложение
+for(;;){ ab -n 50 -c 5 http://localhost/api/v1/cars/all; sleep 3;}
+REM логин и пароль к графане user: admin pass: prom-operator
