a1a2a4deff48ef8d1307a637cfdc01e108af2f6f
Evgeniy Zotov <e.zotov@carx-tech.com>
2021-08-23 12:47:58 +0700

    Удалил модуль dao, перенес репозитории по модулям

--- lab9Saga/app/dictionary/src/main/resources/sql/create_schemas.sql
+++ lab9Saga/app/dictionary/src/main/resources/sql/create_schemas.sql
@@ -1,5 +1,3 @@
 -- Скрипт инициализации schemas при первом запуске
 CREATE SCHEMA IF NOT EXISTS dictionary_schema;
-CREATE SCHEMA IF NOT EXISTS user_schema;
-CREATE SCHEMA IF NOT EXISTS flyway_schema;
 COMMIT;
--- lab9Saga/app/race/src/main/resources/sql/create_schemas.sql
+++ lab9Saga/app/race/src/main/resources/sql/create_schemas.sql
@@ -1,4 +1,3 @@
 -- Скрипт инициализации schemas при первом запуске
 CREATE SCHEMA IF NOT EXISTS race_schema;
-CREATE SCHEMA IF NOT EXISTS flyway_schema;
 COMMIT;
